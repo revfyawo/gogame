c4220360c2d40b107737e68d3aca615cf6bc4cc4
Lucien Haurat <lucien.haurat@free.fr>
2019-01-22 20:02:20 +0100

    using opensimplex noise to generate heatmap

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module gogame
 
-require github.com/veandco/go-sdl2 v0.3.0 // indirect
+require (
+	github.com/ojrac/opensimplex-go v1.0.1 // indirect
+	github.com/veandco/go-sdl2 v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"github.com/ojrac/opensimplex-go"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const chunksX = 5
+const chunksY = 5
+
+func mapInterval(val, min, max, newMin, newMax float64) float64 {
+	return (val-min)*(newMax-newMin)/(max-min) + newMin
+}
 
 func main() {
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
-
 	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
+		chunksX*8*16, chunksY*16*8, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
@@ -19,7 +28,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	surface.FillRect(nil, 0)
+
+	world := make([][]uint32, chunksX*16)
+	for i := range world {
+		world[i] = make([]uint32, chunksY*16)
+	}
+
+	noise := opensimplex.New(1234567890)
+	step := 0.1
+	for i := range world {
+		for j := range world[i] {
+			val := noise.Eval2(float64(i)*step, float64(j)*step)
+			mappedVal := uint32(mapInterval(val, -1, 1, 0, 255))
+			color := 0xff000000 + mappedVal<<16 + mappedVal<<8 + mappedVal
+			world[i][j] = color
+		}
+	}
+
+	for i := range world {
+		for j := range world[i] {
+			surface.FillRect(&sdl.Rect{X: int32(i) * 8, Y: int32(j) * 8, W: 8, H: 8}, world[i][j])
+		}
+	}
 	window.UpdateSurface()
 
 	running := true
