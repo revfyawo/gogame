a20a98755e2c1c716ed8bb67fbc2d94e8265e9f3
Lucien Haurat <lucien.haurat@free.fr>
2019-01-24 19:26:24 +0100

    chunk render system

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/revfyawo/gogame/ecs"
 	"github.com/revfyawo/gogame/engine"
+	"github.com/revfyawo/gogame/systems"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
@@ -13,6 +14,9 @@ func (s *defaultScene) Setup(w *ecs.World) {
 	engine.Input.Register(sdl.SCANCODE_A)
 	engine.Input.Register(sdl.SCANCODE_S)
 	engine.Input.Register(sdl.SCANCODE_D)
+
+	w.AddSystem(&systems.ChunkRender{})
+	w.AddSystem(&systems.Chunks{})
 }
 
 func main() {
---
+++ systems/render.go
@@ -0,0 +1,51 @@
+package systems
+
+import (
+	"github.com/revfyawo/gogame/ecs"
+	"github.com/revfyawo/gogame/engine"
+	"github.com/revfyawo/gogame/entities"
+	"github.com/veandco/go-sdl2/sdl"
+	"time"
+)
+
+type ChunkRender struct {
+	chunks   map[engine.Point]*entities.Chunk
+	messages []*NewChunkMessage
+}
+
+func (c *ChunkRender) PushMessage(m ecs.Message) {
+	mess, ok := m.(*NewChunkMessage)
+	if !ok {
+		return
+	}
+	c.messages = append(c.messages, mess)
+}
+
+func (c *ChunkRender) New() {
+	c.chunks = make(map[engine.Point]*entities.Chunk)
+	engine.Message.Listen(NewChunkMessageType, c)
+}
+
+func (c *ChunkRender) Update(d time.Duration) {
+	for _, m := range c.messages {
+		c.chunks[engine.Point{m.Chunk.Rect.X, m.Chunk.Rect.Y}] = m.Chunk
+	}
+	for point, chunk := range c.chunks {
+		for i := range chunk.Textures {
+			for j := range chunk.Textures[i] {
+				dst := sdl.Rect{
+					X: entities.ChunkSize*int32(point.X) + int32(i)*entities.TileSize,
+					Y: entities.ChunkSize*int32(point.Y) + int32(j)*entities.TileSize,
+					W: entities.TileSize,
+					H: entities.TileSize,
+				}
+				err := engine.Renderer.Copy(chunk.Textures[i][j], nil, &dst)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+func (*ChunkRender) RemoveEntity(e *ecs.BasicEntity) {}
