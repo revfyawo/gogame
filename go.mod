6221920282655ae31271ad2dcfaa9482a1372009
Lucien Haurat <lucien.haurat@free.fr>
2019-01-31 19:15:39 +0100

    added profiling support

--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module github.com/revfyawo/gogame
 
 require (
 	github.com/ojrac/opensimplex-go v1.0.1
+	github.com/pkg/profile v1.2.1
 	github.com/veandco/go-sdl2 v0.3.0
 )
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"flag"
+	"github.com/pkg/profile"
 	"github.com/revfyawo/gogame/ecs"
 	"github.com/revfyawo/gogame/engine"
 	"github.com/revfyawo/gogame/systems"
+	"log"
+	"os"
 )
 
 type defaultScene struct{}
@@ -15,6 +19,18 @@ func (s *defaultScene) Setup(w *ecs.World) {
 	w.AddSystem(&systems.Chunks{})
 }
 
+var cpuprofile = flag.Bool("cpuprofile", false, "profile CPU usage")
+var memprofile = flag.Bool("memprofile", false, "profile memory usage")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile && *memprofile {
+		log.Print("can't use both cpu & memory profiling")
+		os.Exit(1)
+	} else if *cpuprofile {
+		defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	} else if *memprofile {
+		defer profile.Start(profile.MemProfile, profile.ProfilePath(".")).Stop()
+	}
 	engine.Run(&defaultScene{})
 }
