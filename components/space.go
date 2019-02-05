33f2044cfe538fcce62613a1443aaa685a33715c
Lucien Haurat <lucien.haurat@free.fr>
2019-02-05 02:58:01 +0100

    add ChunkTilePosition component

--- components/space.go
+++ components/space.go
@@ -17,7 +17,7 @@ const (
 
 type ChunkPosition struct {
 	Chunk    sdl.Point
-	Position sdl.Point
+	Position sdl.Point // Position in that chunk, in pixels (not scaled)
 }
 
 func (cp *ChunkPosition) MoveX(speed int32) {
@@ -48,4 +48,63 @@ func (cp *ChunkPosition) MoveY(speed int32) {
 	} else {
 		cp.Position.Y = newY
 	}
+}
+
+type ChunkTilePosition struct {
+	Chunk sdl.Point
+	Tile  sdl.Point
+}
+
+func (ctp *ChunkTilePosition) MoveX(tile int32) {
+	newTile := ctp.Tile.X + tile
+	if newTile >= ChunkTile {
+		ctp.Tile.X = newTile % ChunkTile
+		ctp.Chunk.X += int32(math.Floor(float64(newTile) / ChunkTile))
+	} else if newTile < 0 {
+		div := int32(math.Floor(float64(newTile) / ChunkTile))
+		newTile -= div * ChunkTile
+		ctp.Chunk.X += div
+		ctp.Tile.X = newTile
+	} else {
+		ctp.Tile.X = newTile
+	}
+}
+
+func (ctp *ChunkTilePosition) MoveY(tile int32) {
+	newTile := ctp.Tile.Y + tile
+	if newTile >= ChunkTile {
+		ctp.Tile.Y = newTile % ChunkTile
+		ctp.Chunk.Y += int32(math.Floor(float64(newTile) / ChunkTile))
+	} else if newTile < 0 {
+		div := int32(math.Floor(float64(newTile) / ChunkTile))
+		newTile -= div * ChunkTile
+		ctp.Chunk.Y += div
+		ctp.Tile.Y = newTile
+	} else {
+		ctp.Tile.Y = newTile
+	}
+}
+
+func (ctp *ChunkTilePosition) Left() ChunkTilePosition {
+	left := *ctp
+	left.MoveX(-1)
+	return left
+}
+
+func (ctp *ChunkTilePosition) Up() ChunkTilePosition {
+	up := *ctp
+	up.MoveY(-1)
+	return up
+}
+
+func (ctp *ChunkTilePosition) Right() ChunkTilePosition {
+	right := *ctp
+	right.MoveX(1)
+	return right
+}
+
+func (ctp *ChunkTilePosition) Down() ChunkTilePosition {
+	down := *ctp
+	down.MoveY(1)
+	return down
 }
