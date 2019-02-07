b3d4be056bd598af2c08f9e4aadc91278bc30002
Lucien Haurat <lucien.haurat@free.fr>
2019-02-07 17:49:01 +0100

    updates counter texture: generate on rate change, and destroy on disable

--- systems/updatescounter.go
+++ systems/updatescounter.go
@@ -13,13 +13,16 @@ import (
 type UpdatesCounter struct {
 	font        *ttf.Font
 	texture     *sdl.Texture
+	size        sdl.Rect
 	lock        sync.Mutex
 	updateRate  int
 	frameRate   int
+	rateChanged bool
 	updateCount int
 	frameCount  int
 	lastSecond  time.Time
 	enabled     bool
+	disable     bool
 }
 
 func (uc *UpdatesCounter) New(*ecs.World) {
@@ -34,50 +37,69 @@ func (uc *UpdatesCounter) New(*ecs.World) {
 func (uc *UpdatesCounter) UpdateFrame() {
 	uc.lock.Lock()
 	if !uc.enabled {
+		// Destroy texture if just disabled
+		if uc.disable && uc.texture != nil {
+			err := uc.texture.Destroy()
+			if err != nil {
+				panic(err)
+			}
+			uc.texture = nil
+			uc.disable = false
+		}
 		uc.lock.Unlock()
 		return
 	}
 	uc.frameCount++
 	update := uc.updateRate
 	frame := uc.frameRate
+	changed := uc.rateChanged
 	uc.lock.Unlock()
 
-	text := fmt.Sprintf("%v FPS / %v UPS", frame, update)
-	fontSurface, err := uc.font.RenderUTF8Blended(text, sdl.Color{0xff, 0xff, 0xff, 0xff})
-	if err != nil {
-		panic(err)
-	}
-	defer fontSurface.Free()
-
-	surface, err := sdl.CreateRGBSurface(0, fontSurface.W, fontSurface.H, 32, 0xff0000, 0xff00, 0xff, 0xff000000)
-	if err != nil {
-		panic(err)
-	}
-	defer surface.Free()
-	err = surface.FillRect(nil, 0x80000000)
-	if err != nil {
-		panic(err)
-	}
-	err = fontSurface.Blit(nil, surface, &sdl.Rect{0, 0, surface.W, surface.H})
-	if err != nil {
-		panic(err)
-	}
-
-	if uc.texture != nil {
-		err = uc.texture.Destroy()
+	// Regenerate texture if changed, or just enabled
+	if changed || uc.texture == nil {
+		if changed {
+			uc.lock.Lock()
+			uc.rateChanged = false
+			uc.lock.Unlock()
+		}
+		text := fmt.Sprintf("%v FPS / %v UPS", frame, update)
+		fontSurface, err := uc.font.RenderUTF8Blended(text, sdl.Color{0xff, 0xff, 0xff, 0xff})
 		if err != nil {
 			panic(err)
 		}
-		uc.texture = nil
+		defer fontSurface.Free()
+
+		surface, err := sdl.CreateRGBSurface(0, fontSurface.W, fontSurface.H, 32, 0xff0000, 0xff00, 0xff, 0xff000000)
+		if err != nil {
+			panic(err)
+		}
+		defer surface.Free()
+		err = surface.FillRect(nil, 0x80000000)
+		if err != nil {
+			panic(err)
+		}
+		err = fontSurface.Blit(nil, surface, &sdl.Rect{0, 0, surface.W, surface.H})
+		if err != nil {
+			panic(err)
+		}
+
+		if uc.texture != nil {
+			err = uc.texture.Destroy()
+			if err != nil {
+				panic(err)
+			}
+			uc.texture = nil
+		}
+
+		texture, err := engine.Renderer.CreateTextureFromSurface(surface)
+		if err != nil {
+			panic(err)
+		}
+		uc.texture = texture
+		uc.size.W, uc.size.H = surface.W, surface.H
 	}
 
-	texture, err := engine.Renderer.CreateTextureFromSurface(surface)
-	if err != nil {
-		panic(err)
-	}
-	uc.texture = texture
-
-	err = engine.Renderer.Copy(texture, nil, &sdl.Rect{10, 10, surface.W, surface.H})
+	err := engine.Renderer.Copy(uc.texture, nil, &sdl.Rect{10, 10, uc.size.W, uc.size.H})
 	if err != nil {
 		panic(err)
 	}
@@ -88,6 +110,9 @@ func (uc *UpdatesCounter) Update() {
 	defer uc.lock.Unlock()
 	if engine.Input.JustPressed(sdl.SCANCODE_F2) {
 		uc.enabled = !uc.enabled
+		if !uc.enabled {
+			uc.disable = true
+		}
 	}
 	if !uc.enabled {
 		return
@@ -99,6 +124,7 @@ func (uc *UpdatesCounter) Update() {
 		uc.frameRate = uc.frameCount
 		uc.updateCount = 0
 		uc.frameCount = 0
+		uc.rateChanged = true
 	}
 	uc.updateCount++
 }
